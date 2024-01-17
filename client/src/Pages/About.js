852936fc135cded0930ea591e8dd917fa7de07bb
Mohak Tiwari <mohaktiwari2004@gmail.com>
2024-01-17 15:38:04 +0530

    Added Web-Pages

---
+++ client/src/Pages/About.js
@@ -0,0 +1,7 @@
+function AboutUs() {
+    return (
+      <h1>About Page</h1>
+    );
+  }
+  
+  export default AboutUs;
---
+++ client/src/Pages/Join.js
@@ -0,0 +1,7 @@
+function Join() {
+    return (
+      <h1>Join Page</h1>
+    );
+  }
+  
+  export default Join;
