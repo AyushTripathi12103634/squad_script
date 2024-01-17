751767f9bcb16692690ddceeee8c6efedd97a06c
Mohak Tiwari <mohaktiwari2004@gmail.com>
2024-01-18 00:22:30 +0530

    Routing configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
 import Login from './Pages/Login';
-
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Join from './Pages/Join';
+import Profile from './Pages/Profile';
+import Signup from './Pages/Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <h1>Squad Script</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+    </BrowserRouter>
     );
 }
 
