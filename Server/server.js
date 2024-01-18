49bbcbc6628c7dd9a0c4762df9cab64b06109ff8
Ayush Kumar Tripathi <ayushtripathi2003@gmail.com>
2024-01-18 11:34:34 +0530

    usermodel created and register api created with password hashing

--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoute from './routes/authRoute.js';
 
 // Configuring dotenv
 dotenv.config();
@@ -11,6 +12,8 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoute);
 
 app.use("/", (req, res) => {
   res.send("<h1>Squad Script</h1>");
