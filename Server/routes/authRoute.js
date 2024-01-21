9b473c8d9d7309fea77f6af87fd92785c4c1cc9b
Ayush Kumar Tripathi <ayushtripathi2003@gmail.com>
2024-01-21 12:18:59 +0530

    Login api created

--- Server/routes/authRoute.js
+++ Server/routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { registerController } from '../controllers/authcontroller.js';
+import { loginController, registerController } from '../controllers/authcontroller.js';
 const router=express.Router();
 router.post("/register",registerController);
+router.post("/login",loginController);
 export default router;
