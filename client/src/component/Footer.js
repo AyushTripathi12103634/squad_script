882f68fc0c3eeddeb23379fade4bbe459f29af0a
Mohak Tiwari <mohaktiwari2004@gmail.com>
2024-02-01 20:45:14 +0530

    updated footer with links for navigation

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -8,7 +8,8 @@ const Footer = () => {
         <h2 className="text-center" style={{fontSize:'20px',paddingTop:'15px'}}>All Right Reserved &copy; CoffeeCoders</h2>
         <p className="text-center mt-3 mb-0">
           <Link className="footer-links" to="/about">About</Link> | <Link className="footer-links" to="/contact">Contact</Link> |{" "}
-          <Link className="footer-links" to="/policy">Privacy Policy</Link>
+          <Link className="footer-links" to="/privacy-policy">Privacy Policy</Link> |
+          <Link className="footer-links" to="/tnc"> Terms and Conditions</Link>
         </p>
       </div>
     </div>
