914c203d3c560b6b09e2ad2eedffccd33ca2c8f0
Mohak Tiwari <mohaktiwari2004@gmail.com>
2024-02-01 20:49:53 +0530

    created join page

--- client/src/Pages/Join.js
+++ client/src/Pages/Join.js
@@ -1,24 +1,52 @@
+import axios from 'axios';
 import Footer from '../component/Footer';
 import Navbar from '../component/Navbar';
 import './Join.css';
-import React , {useState} from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 function Join() {
-  const [status,setstatus] = useState("Enter meeting link");
+  const [meetid, setmeetid] = useState("");
+  const navigate = useNavigate();
+  const handlemeetid = (e) => {
+    setmeetid(e.target.value);
+  }
+  const join = async (e) => {
+    e.preventDefault();
+    const response = await axios.post(`/api/v1/meet/joinmeet/${meetid}`);
+    navigate(`/meeting/${response.data.meet_id}`);
+  }
+  const create = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/api/v1/meet/createmeet");
+    navigate(`/meeting/${response.data.meeting_id}`);
+  }
   return (
-    <main className='join-main-section'>
+    <>
       <Navbar />
-      <div>
-        <form>
-          <h1>Squad Script Meeting</h1>
-          <input className='form-control' placeholder={status}></input>
-          <select className='form-control'>
-            <option value={"join"}></option>
-            <option value={"join"}></option>
-          </select>
-        </form>
+      <div className='join-main'>
+        <div className='join-content'>
+          <div className='left-container'>
+            <div className='left-heading w-75'>
+              <h1>Connect,Indulge and Develop</h1>
+            </div>
+            <div className='left-content w-50'>
+              <p>Our enhanced premium group project meeting platform, originally designed for secure business engagements as Squad Script, is now available to a broader audience at no cost.</p>
+            </div>
+            <div className='meet-join'>
+              <form className='join-meet'>
+                <input className='form-control w-50' placeholder='Enter meet id' onChange={handlemeetid}></input>
+                <button className='btn join-button' onClick={join}>Join</button>
+              </form>
+              <form className='create-meet'>
+                <button className='btn create-button w-50' onClick={create}>Create a new meeting</button>
+              </form>
+            </div>
+          </div>
+          <div className='right-container ms-5'></div>
+        </div>
       </div>
       <Footer />
-    </main>
+    </>
   );
 }
 
