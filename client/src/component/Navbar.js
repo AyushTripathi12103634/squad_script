61e6e156cfa103d12642b3ef8300dcf1531300b6
Mohak Tiwari <mohaktiwari2004@gmail.com>
2024-02-02 11:00:36 +0530

    added responsiveness in navbar

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,20 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SSLogo from "../images/SquadScript.png";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
-      <div className="Navbar-header px-5 py-3">
-     
-      <Link className="navbar-brand" to="/"><img src={SSLogo} alt=""></img></Link>
-        <ul className="Navbar">
+      <div className='Navbar-header px-5 py-3'>
+        <Link className="navbar-brand" to="/"><img src={SSLogo} alt=""></img></Link>
+        <ul className={`Navbar ${open ? 'open' : ''}`}>
           <li>
             <Link to="/home">Home</Link>
           </li>
           <li>
-            <Link to="/about"> About us</Link>
+            <Link to="/about">About us</Link>
           </li>
           <li>
             <Link to="/contact">Contact Us</Link>
@@ -29,7 +34,7 @@ const Navbar = () => {
             <i className="ri-user-fill" />
             Sign In / Register
           </Link>
-          <div className="bx bx-menu" id="Navbar-menu-icon" />
+          <div className={`bx bx-menu ${open ? 'bx-x' : ''}`} id="Navbar-menu-icon" onClick={handleClick} />
         </div>
       </div>
     </>
