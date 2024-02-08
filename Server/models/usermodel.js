40eef42fdf3acd1388b13650f94fce583aa73255
Ayush Kumar Tripathi <ayushtripathi2003@gmail.com>
2024-02-08 11:00:05 +0530

    Added feature to prevent use of Disposable mail address

--- Server/models/usermodel.js
+++ Server/models/usermodel.js
@@ -28,10 +28,28 @@ const userschema = new mongoose.Schema(
       required: true,
       default:false,
     },
+    lastVerified: {
+      type: Date,
+      default: Date.now,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+userschema.methods.checkVerification = function() {
+  const sixMonthsAgo = new Date();
+  sixMonthsAgo.setMonth(sixMonthsAgo.getMonth() - 6);
+  const oneMonthAgo = new Date();
+  oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
+  if (this.lastVerified < sixMonthsAgo) {
+    this.isVerified = false;
+    this.save();
+  }
+  if (!this.isVerified && this.lastVerified < oneMonthAgo) {
+    this.remove();
+  }
+};
 
 export default mongoose.model("Users", userschema);
--- Server/server.js
+++ Server/server.js
@@ -8,6 +8,7 @@ import rapidApiRoute from './routes/rapidApiRoute.js'
 import cors from 'cors';
 import { Server } from "socket.io";
 import { createServer } from "http";
+import './cronjob/cronJobs.js';
 
 // Configuring dotenv
 dotenv.config();
@@ -24,7 +25,7 @@ const io = new Server(server, {
   },
 });
 
-io.on('connection', (socket) => {
+io.on('connection',(socket) => {
   console.log('a new user connected', socket.id);
 
   socket.on('join room', (room) => {
@@ -33,11 +34,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('code', (data) => {
-    socket.to(data.room).emit('code', data.text);
-  });
-
-  socket.on('message', (data) => {
-    socket.to(data.room).emit("broadcast", data.text);
+    socket.in(data.room).emit('code', data.text);
   });
 
   socket.on('disconnect', () => {
@@ -55,7 +52,7 @@ app.use("/", (req, res) => {
   res.send("<h1>Squad Script</h1>");
 });
 
-const PORT = process.env.PORT || 5000; // Assuming a default port of 5000
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port number ${PORT}`.bgGreen.white);
