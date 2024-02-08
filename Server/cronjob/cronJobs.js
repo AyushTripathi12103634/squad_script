552a6a1e974f539f504306472d80f90237ed6c11
Ayush Kumar Tripathi <111583605+AyushTripathi12103634@users.noreply.github.com>
2024-02-08 11:32:36 +0530

    Update cronJobs.js

--- Server/cronjob/cronJobs.js
+++ Server/cronjob/cronJobs.js
@@ -1,9 +1,12 @@
 import cron from 'node-cron';
 import usermodel from '../models/usermodel.js'
 
-cron.schedule('*/10 * * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
     try {
-        console.log("hello");
+        const users = await usermodel.find();
+        users.forEach(user => {
+            user.checkVerification();
+        });
     } catch (error) {
         console.error(error);
     }
