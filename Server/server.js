21b455f55e7cbe57aaeb2a30398f824289e492a3
Ayush Kumar Tripathi <111583605+AyushTripathi12103634@users.noreply.github.com>
2024-08-10 19:26:29 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.SERVER_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true,
   },
